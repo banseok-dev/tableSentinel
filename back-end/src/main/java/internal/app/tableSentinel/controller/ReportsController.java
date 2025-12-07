657b710c60b0526273880963bdb629d67d0dbf90
banseok-dev <banseok-dev@outlook.com>
2025-12-07 23:25:07 +0900

    pre(back-end): Reports 처리부분 준비중 fix(gradle): 프로젝트 설명 변경

--- back-end/src/main/java/internal/app/tableSentinel/controller/ReportsController.java
+++ back-end/src/main/java/internal/app/tableSentinel/controller/ReportsController.java
@@ -1,18 +1,19 @@
 package internal.app.tableSentinel.controller;
 
+import internal.app.tableSentinel.dto.ReportsDto;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller 
+@Controller
 public class ReportsController {
-
     @ResponseBody
     @PostMapping("/api/agents/{agentId}/reports")
     public String receiveAgentReport(
-        @PathVariable("agentId") String agentId)
-        {
+        @PathVariable("agentId") String agentId
+    ) {
         System.out.println("보고서 수신 (에이전트 ID): " + agentId);
         return "Report received for " + agentId;
     }
---
+++ back-end/src/main/java/internal/app/tableSentinel/dto/ReportsDto.java
@@ -0,0 +1,13 @@
+package internal.app.tableSentinel.dto;
+import lombok.Data;
+
+@Data
+public class ReportsDto {
+    // 데이터 모델링 후 개선 필요
+    private String family;
+    private String table;
+    private String chain;
+    private String name;
+    private String ipAddress;
+    
+}
